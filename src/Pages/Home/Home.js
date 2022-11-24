a5ad1933ce2317378af127577d817eb736ad6153
lalon147 <lalon.rahman147@gmail.com>
2022-11-24 15:44:13 +0600

    footer added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,28 +1,16 @@
-import React, { useState } from 'react';
+
 import Banner from './Banner';
+import Categories from './Categories';
+
 
 const Home = () => {
+    console.log(new Date())
      
     return (
         <div>
              <Banner></Banner>
-              <nav className=''>
-                <h3 className='font-bold'>COMPANY:</h3>
-                 <ul className='list-none'>
-                 <li>AUDI</li>
-                 <li>TOYOTA</li>
-                 <li>TESLA</li>
-                 </ul>
-                <h3 className='font-bold'>CATEGORY</h3>
-                 <ul className='list-none'>
-                 <li>SUV</li> 
-                <li>CONVERTABLE</li> 
-                <li>SEDAN</li>
-                 </ul>
-                <h3 className='font-bold'>PRICE RANGE</h3>
-                <input defaultValue="100,0000" className='input input-bordered' placeholder='ENTER MINIMUM PRICE'/>
-                <input defaultValue="500,0000"  className='input input-bordered' placeholder='ENTER MAXIMUM PRICE PRICE'/>
-              </nav>
+             <Categories></Categories>
+              
         </div>
     );
 };
