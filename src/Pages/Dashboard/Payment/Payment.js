96b1a7cac6db65ff6a2080a882ad6ffa7fe87281
lalon147 <lalon.rahman147@gmail.com>
2022-11-25 15:13:59 +0600

    payment

--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -2,18 +2,13 @@ import React from 'react';
 import {loadStripe} from "@stripe/stripe-js";
 import {Elements} from "@stripe/react-stripe-js"
 import { useLoaderData } from 'react-router-dom';
-import CheckoutForm from './CheckoutForm';
+
+import CheckoutFomr from './CheckoutForm';
 const stripePromise = loadStripe('pk_test_51M6IHMCSwhlczGM7RDTg0jGs4nH0DRM1jAvm9e0lk4DZ5XMmHU92whRf6Pw5bCUqQLqNPjDhMkDn18QUHvtkfKS300tgJfbydv');
 
 
 
-const ELEMENTS_OPTIONS = {
-    fonts: [
-      {
-        cssSrc: 'https://fonts.googleapis.com/css?family=Roboto',
-      },
-    ],
-  };
+
 const Payment = () => {
      const data=useLoaderData();
      console.log(data)
@@ -21,9 +16,11 @@ const Payment = () => {
         <div>
              <h3 className='text-3xl'>PAYMENT FOR {data.carName}</h3>
              <p className='font-bold text-2xl'>TOTAL COST: {data.carPrice}BDT</p>
-             <Elements stripe={stripePromise} options={ELEMENTS_OPTIONS}>
-                 <CheckoutForm data={data} />
-             </Elements>
+             <Elements stripe={stripePromise}>
+                    <CheckoutFomr
+                     data={data}
+                    ></CheckoutFomr>
+                  </Elements>
         </div>
     );
 };
